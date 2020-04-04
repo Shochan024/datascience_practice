a871b88d0b54563d51e7ef03d7bded3512338695
jockroiddev@gmail.com <jockroiddev@gmail.com>
2020-04-04 19:12:01 +0900

    First Commit

--- simple_bayes.py
+++ simple_bayes.py
@@ -14,11 +14,13 @@ p(spam|meeting)=p(meeting|spam)*p(spam) / p(meeting)
 = p(meeting|spam)*p(spam) + p(meeting|ham)*p(ham)
 """
 
+spam_text = "meeting" #ここの値を変えると確率が変わる。それ以外は触らないくて良い
+
 #基本情報取得( N_spam , N_ham , N )
 spam = glob.glob("datasets/enron1/spam/*")
 ham = glob.glob("datasets/enron1/ham/*")
-text_spam = lib.txt_count( files=spam , text="meeting" , encoding="latin-1" )
-text_ham = lib.txt_count( files=ham , text="meeting" , encoding="latin-1" )
+text_spam = lib.txt_count( files=spam , text=spam_text , encoding="latin-1" )
+text_ham = lib.txt_count( files=ham , text=spam_text , encoding="latin-1" )
 N = text_ham["N"] + text_spam["N"]
 
 #各確率の算出( p(spam) , p(ham) )
@@ -34,4 +36,4 @@ p_meeting = p_meeting_bar_spam*p_spam + p_meeting_bar_ham*p_ham
 
 #p(spam|meeting)
 p_spam_meeting = round( (p_meeting_bar_spam*p_spam) / p_meeting ,2 )
-print( "p(spam|meeting)="+str(p_spam_meeting)+"≒"+str(round(p_spam_meeting*100))+"%" )
+print( "p(spam|"+spam_text+")="+str(p_spam_meeting)+"≒"+str(round(p_spam_meeting*100))+"%" )
